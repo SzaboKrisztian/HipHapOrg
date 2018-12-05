78b9d8c8c1576102453dd2dec87b6bc27e55ae93
Chris Szabo <szabo.krisztian.87@gmail.com>
2018-12-05 12:53:45 +0100

    Implemented Person management options

--- src/org/hiphap/Screens/PersonView.java
+++ src/org/hiphap/Screens/PersonView.java
@@ -1,6 +1,7 @@
 package org.hiphap.Screens;
 
 import org.hiphap.Person;
+import org.hiphap.PersonManager;
 
 public class PersonView extends MenuScreen {
   private Person currentPerson;
@@ -8,8 +9,44 @@ public class PersonView extends MenuScreen {
   Transition handleInput(String input) {
     switch (input) {
       case "1":
+        String firstName = null;
+        do {
+          clearScreen();
+          System.out.println("Old name: " + currentPerson.toString());
+          if (firstName != null && firstName.equals("")) {
+            System.out.println("First name cannot be blank.");
+          }
+          firstName = readString("Input first name: ");
+        } while (firstName.equals(""));
+        String lastName = readString("Input last name: ");
+        String middleName = readString("Input middle name: ");
+        currentPerson.setFirstName(firstName);
+        currentPerson.setLastName(lastName);
+        currentPerson.setMiddleName(middleName);
+        return new Transition(Transition.Type.SUCCESS, "Person name successfully changed.");
       case "2":
-        return new Transition(Transition.Type.INVALID, "Not implemented yet");
+        clearScreen();
+        System.out.println("Old email address: " + (currentPerson.getEmail().equals("") ?
+            "N/A" : currentPerson.getEmail()));
+        currentPerson.setEmail(readString("Input new email address: "));
+        return new Transition(Transition.Type.SUCCESS, "Email address successfully changed.");
+      case "3":
+        clearScreen();
+        System.out.println("Old phone number: " + (currentPerson.getPhone().equals("") ?
+            "N/A" : currentPerson.getPhone()));
+        currentPerson.setPhone(readString("Input new phone number: "));
+        return new Transition(Transition.Type.SUCCESS, "Phone number successfully changed.");
+      case "4":
+        if (clsAndReadBoolean("WARNING! Operation cannot be undone. Are you " +
+            "sure you wish to delete this person?")) {
+          if (PersonManager.getInstance().deletePerson(currentPerson)) {
+            return new Transition(Transition.Type.BACK, "Person successfully deleted.");
+          } else {
+            return new Transition(Transition.Type.INVALID, "Error deleting person.");
+          }
+        } else {
+          return new Transition(Transition.Type.INVALID, "Operation aborted.");
+        }
       default:
         return new Transition(Transition.Type.INVALID, "Invalid input; try again.");
     }
@@ -18,12 +55,16 @@ public class PersonView extends MenuScreen {
   public PersonView(Person person) {
     this.currentPerson = person;
     this.setMenuNode(true);
-    addMenuOption("1", "Edit person details");
-    addMenuOption("2", "Delete person");
+    addMenuOption("1", "Edit person name");
+    addMenuOption("2", "Edit email address");
+    addMenuOption("3", "Edit phone number");
+    addMenuOption("4", "Delete person");
   }
 
   public void showContent() {
-    printPadding();
-    System.out.println("Person name: " + currentPerson);
+    clearScreen();
+    System.out.println("Person name: " + currentPerson.toString());
+    System.out.println("Email address: " + currentPerson.getEmail());
+    System.out.println("Phone number: " + currentPerson.getPhone());
   }
 }
